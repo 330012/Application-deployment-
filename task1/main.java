0553c6790ef47cadcf2eccc0540550951b2ef233
gautav <gauravkhunt110@gmail.com>
2024-01-16 19:53:23 +0530

    new a

--- task1/main.java
+++ task1/main.java
@@ -1,6 +1,6 @@
 public class main {
     public static void main(String[] args) {
         System.err.println("hello");
-        System.out.println("hello from the gaurav");
+        System.out.println("hello from the gaurav ronak");
     }
 }
